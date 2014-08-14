9eaca4924238413fa520f46bffd570d2b4b21ac3
Marcelo J. Both <marcelo.both@gmail.com>
2014-08-14 07:09:29 -0300

    First commit

---
+++ spec/models/pokemon_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Pokemon do
+
+  it { should be_a(ActiveRecord::Base)  }
+
+  describe '#nome_completo' do
+
+   context 'quando possui nome e o id nacional' do
+
+     subject(:pokemon) do
+       Pokemon.new(nome: 'Charizard', id_nacional: 6)
+     end
+
+      it 'exibe o nome e o id nacional' do
+        expect(pokemon.nome_completo).to eq('Charizard - 6')
+      end
+    end
+
+    context 'quando não possui o nome e o id nacional' do
+
+      subject do
+        Pokemon.new
+      end
+
+      it 'é nil' do
+        expect(subject.nome_completo).to be_nil
+      end
+    end
+  end
+end
